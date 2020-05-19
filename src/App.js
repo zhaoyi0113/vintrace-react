f3e62a3b090f46b2c38dd239f5e34c1c618f8490
joey <zhaoyi0113@gmail.com>
2020-05-19 15:25:36 +1000

    show wine list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from '@material-ui/core';
+import {LocalDrink} from '@material-ui/icons';
 
 import './App.css';
 
 const WineList = ({ wineryData }) => {
   return (
-    <div>
-      {wineryData.map((data) => (
-        <div>{data.lotCode}</div>
+    <List>
+      {wineryData.map((data, i) => (
+        <ListItem key={`item-${i}`} style={{ cursor: 'pointer' }}>
+          <ListItemAvatar>
+            <LocalDrink />
+          </ListItemAvatar>
+          <ListItemText primary={data.lotCode} secondary={data.description} />
+        </ListItem>
       ))}
-    </div>
+    </List>
   );
 };
 
