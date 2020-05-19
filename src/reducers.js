3effb448316d20bf2f2919cf2ec3275c60081877
joey <zhaoyi0113@gmail.com>
2020-05-19 14:43:52 +1000

    init commit

---
+++ src/reducers.js
@@ -0,0 +1,19 @@
+import { combineReducers } from 'redux'
+
+function counter(state, action) {
+  if (typeof state === 'undefined') {
+    state = 0 // If state is undefined, initialize it with a default value
+  }
+
+  if (action.type === 'INCREMENT') {
+    return state + 1
+  } else if (action.type === 'DECREMENT') {
+    return state - 1
+  } else {
+    return state // In case an action is passed in we don't understand
+  }
+}
+
+export const rootReducer = combineReducers({
+  counter
+});
